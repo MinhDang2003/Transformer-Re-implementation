101eb9e714aebc483abaae9e72523270bfd8adcb
Minh Dang <dangquocminh2017@gmail.com>
2024-08-26 17:41:50 +0700

    Add files via upload
    add functions to config.py
    change preload to None

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import yaml
+from pathlib import Path
 
 def get_config(config_file='config.yaml'):
     try:
@@ -23,3 +24,17 @@ def update_config(new_settings: dict={}, config_file: str='config.yaml'):
         yaml.dump(config, file, default_flow_style=False)
         
     return
+
+def get_weights_path(config, epoch: str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return str(Path('.') / model_folder / model_filename)
+
+def get_latest_weight(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
